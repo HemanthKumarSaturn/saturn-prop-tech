40c446f366eaee9676916c6f5749399ed34f4189
HemanthKumarSaturn <hemanth.saturnrealcon@gmail.com>
2024-04-20 10:01:34 +0530

    Add Width to 100%

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -10,7 +10,7 @@ function Banner() {
       style={{
         backgroundImage:
           "url(https://cdn.builder.io/api/v1/image/assets/TEMP/abf422d88a7326565f89a01b2d7c7ce016caf4edb3a33a0151f3e9717135a60a?apiKey=6d29d163b59344eda42474c42f65e83e&)",
-        maxWwidth: "100vw",
+        maxWidth: "100%",
         height: "88vh",
         aspectRatio: "1.82",
         backgroundSize: "cover",
